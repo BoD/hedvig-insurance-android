9e09d02aa025cb2a92fee5d621c04b2e0fdd14b0
BoD <BoD@JRAF.org>
2024-07-03 17:15:05 +0200

    Fix missing com.apollographql.apollo.api.Error import

--- app/app/src/main/kotlin/com/hedvig/android/app/apollo/DatadogInterceptor.kt
+++ app/app/src/main/kotlin/com/hedvig/android/app/apollo/DatadogInterceptor.kt
@@ -2,6 +2,7 @@ package com.hedvig.android.app.apollo
 
 import com.apollographql.apollo.api.ApolloRequest
 import com.apollographql.apollo.api.ApolloResponse
+import com.apollographql.apollo.api.Error
 import com.apollographql.apollo.api.Operation
 import com.apollographql.apollo.interceptor.ApolloInterceptor
 import com.apollographql.apollo.interceptor.ApolloInterceptorChain
